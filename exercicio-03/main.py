0773142b9049e53594384b61bf6d4d40118692b6
Pedro Calderón <pedrocalderonn52@gmail.com>
2024-11-04 20:54:36 -0300

    exercicio 3 resolvido

--- exercicio-03/main.py
+++ exercicio-03/main.py
@@ -1,1 +1,9 @@
-print('teste')
+import turtle as tl
+from ponto import Ponto
+
+ponto = Ponto(300, 200, "green", 7)
+tl.color(ponto.cor)
+tl.width(ponto.grossura)
+tl.goto(ponto.x, ponto.y)
+
+tl.done()
---
+++ exercicio-03/ponto.py
@@ -0,0 +1,10 @@
+import turtle as tl
+class Ponto:
+    def __init__(self, x, y, cor, grossura):
+        self.x = x
+        self.y = y
+        self.cor = cor
+        self.grossura = grossura
+    
+
+        
