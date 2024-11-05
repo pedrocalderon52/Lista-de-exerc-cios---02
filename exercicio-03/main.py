e686d2cf03d5e16ae80bfc4cfc248d54cd79b109
Pedro Calderón <pedrocalderonn52@gmail.com>
2024-11-05 11:38:20 -0300

    adicionada feature de deixar a linha tracejada (com correção do for e utilizando o módulo math)

--- exercicio-03/main.py
+++ exercicio-03/main.py
@@ -1,9 +1,20 @@
 import turtle as tl
 from ponto import Ponto
 
-ponto = Ponto(300, 200, "green", 7)
-tl.color(ponto.cor)
-tl.width(ponto.grossura)
-tl.goto(ponto.x, ponto.y)
 
+ponto = Ponto(-300, 200, "green", 7)
+ponto.andar_tracejado()
+ponto.set_ponto(300, 150)
+ponto.andar_tracejado()
+ponto.set_ponto(0, 0)
+ponto.andar_tracejado()
+
+
+
+
+
+# vai ser adicionada uma opção de deixar a linha tracejada, fazer com pitágoras entre x e y
+# distancia = raiz de ((x0 - x)² + (y0 - y)²)
+# use trigonometria para descobrir o angulo
+# a cada 2 cm, mais ou menos, ande com pen up, ande com pen down.
 tl.done()
--- exercicio-03/ponto.py
+++ exercicio-03/ponto.py
@@ -1,10 +1,69 @@
 import turtle as tl
+import math as m
 class Ponto:
     def __init__(self, x, y, cor, grossura):
-        self.x = x
-        self.y = y
+        self.prox_x = x
+        self.prox_y = y
         self.cor = cor
         self.grossura = grossura
+        self.x = 0
+        self.y = 0
+        tl.color(self.cor)
+        tl.width(grossura)
+
+
+    def set_cor(self, cor):
+        tl.color(cor)
+        self.cor = cor
+
+
+    def set_grossura(self, grossura):
+        self.grossura = grossura
+        tl.width(grossura)
+
+
+    def set_ponto(self, x, y):
+        self.prox_x = x
+        self.prox_y = y
+
+
+    def andar_reto(self):
+        tl.goto(self.prox_x, self.prox_y)
+        self.x = self.prox_x
+        self.y = self.prox_y
+
+
+    def andar_tracejado(self):
+        adjacente =  self.prox_x - self.x
+        oposto = self.prox_y - self.y
+
+        distancia = m.sqrt(adjacente ** 2 + oposto ** 2)
+        
+        angulo = m.degrees(m.atan(abs(oposto)/abs(adjacente)))
+        if adjacente < 0: 
+            if oposto < 0:
+                tl.left(180 + angulo)
+            else:
+                tl.left(180-angulo)
+        else:
+            if oposto < 0:
+                tl.left(0 - angulo)
+            else:
+                tl.left(0 + angulo)
+        tl.fd(10)
+        for _ in range(int(distancia // 20 - ((abs(10 - distancia % 20)) / 20))):
+            tl.pendown()
+            tl.fd(10)
+            tl.penup()
+            tl.fd(10)
+        tl.pendown()
+        tl.goto(self.prox_x, self.prox_y)
+        self.x = self.prox_x
+        self.y = self.prox_y
+        tl.setheading(0)
+
+        
+
     
 
         
